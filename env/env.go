0b2f81c3e24bb847ab4b5d3fa3a28be799e7423f
AeroGenCreator <gallo85floyd@gmail.com>
2026-08-29 09:17:22 -0600

    Log MSG Edited

--- env/env.go
+++ env/env.go
@@ -15,7 +15,7 @@ func LoadGlobalEnvs() error {
 		log.Printf("Invalid value given to 'IS_PRODUCTION' (%v): default 'false'", err)
 		IsProduction = false
 	}
-	log.Print("Project 'IS_PRODUCTION' status set to: ", IsProduction)
+	log.Print("PROJECT STATUS SET TO:", IsProduction)
 
 	ApyKey = (os.Getenv("JWT_KEY"))
 
